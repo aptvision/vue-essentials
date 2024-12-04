5bbac9af75054e423d963e3c71cb4757f1cf3de9
scg-hub <grzegorz.sic@scghub.com>
2024-12-04 23:01:00 +0100

    fixes

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.greet = void 0;
-const greet = (name) => `Hello, ${name}!`;
-exports.greet = greet;
+export function useTestHelpers(options) {
+    console.log(options);
+    const greet = (name) => `Hello111, ${name}!`;
+    return {
+        greet
+    };
+}
+const { greet } = useTestHelpers();
+console.log(greet('GREG'));
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export const greet = (name: string) => `Hello, ${name}!`;
+
+
+export function useTestHelpers(options?:any) {
+    console.log(options)
+    const greet = (name: string) => `Hello111, ${name}!`
+
+    return {
+        greet
+    }
+}
+
+const {greet} = useTestHelpers()
+
+console.log(greet('GREG'))
