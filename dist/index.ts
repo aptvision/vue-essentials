df9d18afb90a79dcd51de8adfac4a71b35cabffa
scg-hub <grzegorz.sic@scghub.com>
2024-12-05 13:52:53 +0100

    changes

--- dist/index.ts
+++ dist/index.ts
@@ -1,5 +1,5 @@
 import { useDateHelpers } from "./core/DateHelpers"
 
-export type {
+export {
     useDateHelpers
 }
--- dist/types/index.ts
+++ dist/types/index.ts
@@ -1,5 +1,5 @@
 import { IDateHelpersConfig } from "./DateHelpersInterface";
 
-export {
+export type{
     IDateHelpersConfig
 }
