2a0f8d5ab44faf9107271418c2666811bd64e3de
scg32 <grzes33@gmail.com>
2024-12-09 22:55:58 +0100

    test

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,5 @@
-import { IDateHelpersConfig } from "./DateHelpersInterface";
-import { TRestApiOptionsOverride, ApiErrorInterface, RestApiResponseInterface, ResponseMeta } from '../core/ApiRest';
-type JsonObject<T = unknown> = {
-    [Key in string]?: T;
-};
-type TScalar = string | number | boolean | string[] | number[];
-type NonEmptyString<T> = T extends '' ? never : T;
-export type { IDateHelpersConfig, TRestApiOptionsOverride, JsonObject, RestApiResponseInterface, ResponseMeta, ApiErrorInterface, TScalar, NonEmptyString };
+import { useDateHelpers } from "./core/DateHelpers";
+import { useApiRest } from './core/ApiRest';
+import AptError from './core/AptError';
+import { useTranslate } from './core/Translate';
+export { useDateHelpers, useApiRest, AptError, useTranslate };
