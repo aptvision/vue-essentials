cfb9087cff8952b148e75929a35a2dc2d304e737
scg32 <grzes33@gmail.com>
2024-12-09 19:05:58 +0100

    test

---
+++ core/Translate.d.ts
@@ -0,0 +1,1 @@
+export declare const useTranslate: (localeCode: string, translations: Record<string, Record<string, string>>) => (msg: string, params?: Record<string, string>) => string;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { useDateHelpers } from "./core/DateHelpers";
+import { useApiRest } from './core/ApiRest';
+import AptError from './core/AptError';
+import { useTranslate } from './core/Translate';
+export { useDateHelpers, useApiRest, AptError, useTranslate };
