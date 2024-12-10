9f9a719928f5be42d6aad3d199e2883cec405c80
scg32 <grzes33@gmail.com>
2024-12-10 17:02:01 +0100

    build

--- prod/quasar/quasar-init.js
+++ prod/quasar/quasar-init.js
@@ -8,7 +8,7 @@ const app = createApp({
 app.use(Quasar, {
     plugins: { Notify, Dialog },
 });
-const div = document.createElement('div');
-document.body.appendChild(div);
-app.mount(div);
+// const div = document.createElement('div')
+// document.body.appendChild(div);
+app.mount('#app-vue-essentials');
 export { app };
