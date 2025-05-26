627aec5455640007bf0ecb64d48909c32c1418ce
scg32 <grzes33@gmail.com>
2025-05-26 15:55:43 +0200

    fix

--- src/core/DateHelpers.ts
+++ src/core/DateHelpers.ts
@@ -61,7 +61,7 @@ export function useDateHelpers (config?:IDateHelpersConfig):IUseDateHelpersRetur
     
     const dtf = new Intl.DateTimeFormat(localeCode, options);
   
-    const tokenMap = { year: 'yyyy', month: 'MM', day: 'dd' }
+    const tokenMap = { year: 'yyyy', month: 'mm', day: 'dd' }
   
     return dtf.formatToParts(sampleDate)
       .map(part => part.type === 'literal'
@@ -84,7 +84,7 @@ export function useDateHelpers (config?:IDateHelpersConfig):IUseDateHelpersRetur
     
     const dtf = new Intl.DateTimeFormat(localeCode, options);
   
-    const tokenMap = { year: 'yyyy', month: 'MM', day: 'dd', hour: 'HH', minute: 'mm' }
+    const tokenMap = { year: 'yyyy', month: 'mm', day: 'dd', hour: 'HH', minute: 'mm' }
   
     return dtf.formatToParts(sampleDate)
       .map(part => part.type === 'literal'
@@ -108,7 +108,7 @@ export function useDateHelpers (config?:IDateHelpersConfig):IUseDateHelpersRetur
     
     const dtf = new Intl.DateTimeFormat(localeCode, options);
   
-    const tokenMap = { year: 'yyyy', month: 'MM', day: 'dd', hour: 'HH', minute: 'mm', second: 'ss' }
+    const tokenMap = { year: 'yyyy', month: 'mm', day: 'dd', hour: 'HH', minute: 'mm', second: 'ss' }
   
     return dtf.formatToParts(sampleDate)
       .map(part => part.type === 'literal'
